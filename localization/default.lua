67fc641a49b28cd94595ea7794374278fecf5ed0
TheOneGoofAli <alilesha552@gmail.com>
2025-09-02 03:26:40 +0300

    1.9.3a1 (optional Bunco DeckSkins)
    Bunco's Fleuron and Halberd suits now available in "Classic Windows Cards" style.

--- localization/default.lua
+++ localization/default.lua
@@ -1857,6 +1857,8 @@ return {
 			toga_oldschool_Hearts = { "Original", "Low Contrast (Recoloured)", "High Contrast (Recoloured)", },
 			toga_oldschool_Clubs = { "Original", "Low Contrast (Recoloured)", "High Contrast (Recoloured)", },
 			toga_oldschool_Diamonds = { "Original", "Low Contrast (Recoloured)", "High Contrast (Recoloured)", },
+			toga_oldschool_bunc_Fleurons = { "Low Contrast (Recoloured)", "High Contrast (Recoloured)", },
+			toga_oldschool_bunc_Halberds = { "Low Contrast (Recoloured)", "High Contrast (Recoloured)", },
 			toga_modern_Spades = { "Classic", "Hearts Deck", "Seasons Deck", "Large Print Deck", },
 			toga_modern_Hearts = { "Classic", "Hearts Deck", "Seasons Deck", "Large Print Deck", },
 			toga_modern_Clubs = { "Classic", "Hearts Deck", "Seasons Deck", "Large Print Deck", },
